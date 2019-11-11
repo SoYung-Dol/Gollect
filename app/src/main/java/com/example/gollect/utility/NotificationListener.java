58181e8479ef500ecd54245400b40891f45f137b
DAWOON <rhdrlvnd@ajou.ac.kr>
2019-11-11 20:48:58 +0900

    알람이 올 때 db에 추가

--- app/src/main/java/com/example/gollect/utility/NotificationListener.java
+++ app/src/main/java/com/example/gollect/utility/NotificationListener.java
@@ -7,6 +7,10 @@ import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
 
+import com.example.gollect.AlarmData;
+
+import java.util.Date;
+
 import io.realm.Realm;
 
 public class NotificationListener extends NotificationListenerService {
@@ -44,6 +48,45 @@ public class NotificationListener extends NotificationListenerService {
                 " title: " + title +
                 " text : " + text +
                 " subText: " + subText);
+
+        //Realm에 객체(데이터) 저장
     }
 
+
+    private void addAlarm(String appName, String contents, Date date){
+        date.setTime(System.currentTimeMillis());
+        final AlarmData AlarmData = new AlarmData(getAlarmDataId(), appName, contents, date);
+
+        realm.executeTransactionAsync(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                //Realm에 생성한 다이어리를 저장하는 코드
+                realm.copyToRealm(AlarmData);
+                // 저장한 다이어리를 다이어리 리스트에 담아주는코드
+                //AlarmDataList.add(AlarmData);
+                //AlarmDataAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    //Realm에 저장할 Diary의 ID를 자동으로 증가시켜서 가져오는 메소드
+    private int getAlarmDataId() {
+        //자동으로 Id를 증가시켜야 되기 때문에
+        //이 메소드가 불린 시점에서 Realm에 저장되어있는 Diary의 ID의 최대값을 구해옵니다.
+        Number currentId = realm.where(AlarmData.class).max("id");
+
+        //새로 저장할 Diary의 ID값
+        int nextId;
+
+        //Realm에 Diary가 저장되어있지 않아 저장된 ID도 없는 경우에는
+        if (currentId == null) {
+            //처음 생성되는 ID이기 때문에 1을 지정합니다
+            nextId = 1;
+            //Realm에 Diary가 저장되어있는 경우에는 저장되어 있는 Diary의 최대 ID를 찾아와서
+        } else {
+            //찾아온 ID에 +1을 해서 돌려줍니다.
+            nextId = currentId.intValue() + 1;
+        }
+        return nextId;
+    }
 }
