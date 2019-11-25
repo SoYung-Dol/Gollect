6c02db1a5e2b4f0c9ba4c71c94490c2c40f1c065
DAWOON <rhdrlvnd@ajou.ac.kr>
2019-11-25 15:19:50 +0900

    Update button

--- app/src/main/java/com/example/gollect/SubscibeActivity.java
+++ app/src/main/java/com/example/gollect/SubscibeActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -31,7 +32,15 @@ public class SubscibeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subscribe);
+        Button completeBt = findViewById(R.id.completeBt);
         //전체 Diary 목록을 Realm에 요청해서 받아오는 코드입니다
+
+        completeBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                subscribeSuccess();
+            }
+        });
 
         getPlatforms();
     }
